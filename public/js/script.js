cb0b09361a0a15f48f551c218c6033b936e10ffc
Melissa Steffan <msteffan2012@gmail.com>
2015-08-24 14:16:24 -0400

    script.js has ajax to bandsintown and echonest

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,13 @@
 $(".currentArtist").on("click", function(){
     var artist = $(".currentArtist").html()
-    $.getJSON("http://api.bandsintown.com/artists/"+artist+"/events.json?api_version=2.0&app_id=YOUR_APP_ID", function(response){
+    $.getJSON("http://api.bandsintown.com/events/recommended?artists[]="+artist+"&location=use_geoip&format=json&app_id=YOUR_APP_ID", function(response){
         console.log(response);
     })
+    $.getJSON("http://developer.echonest.com/api/v4/artist/news?api_key=6N51VGIQONFDX0AGP&name="+artist+"&format=json", function(response){
+        console.log(response);
+    })
+
 })
+
+
+//6N51VGIQONFDX0AGP
