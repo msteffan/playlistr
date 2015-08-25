ec921f7c07e11269ee7ece18ac654f1d148c6862
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-08-25 10:58:24 -0400

    morning

--- public/js/script.js
+++ public/js/script.js
@@ -11,15 +11,16 @@ $("#makePlaylist").on("click", function(){
     var songCount = $("#songCount").val()
     $.getJSON("http://developer.echonest.com/api/v4/playlist/basic?api_key=6N51VGIQONFDX0AGP"+artistCode+"&format=json&results="+songCount+"&bucket=tracks&bucket=id:spotify", function(response){
         console.log(response);
-        tracks = [];
+        var tracks = [];
         for(i=0;i < response.response.songs.length; i++){
             var track = response.response.songs[i]["tracks"][0]["foreign_id"]
             var newString = track.substr(14);
             console.log(track);
             tracks.push(newString);
         }
-        var ids = tracks.join()
-        $(".currentArtist").append('<iframe id="musicframe" src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:'+ids+'" frameborder="0" height="800" width="400" allowtransparency="true"></iframe>')
+        var ids = tracks.join();
+        var playlistName = $("#listName").val();
+        $(".currentArtist").append('<iframe id="musicframe" src="https://embed.spotify.com/?uri=spotify:trackset:'+playlistName+':'+ids+'" frameborder="0" height="800" width="400" allowtransparency="true"></iframe>')
     })
     var bandCode = "";
     for (i = 0; i < artist.length; i++) {
@@ -29,7 +30,12 @@ $("#makePlaylist").on("click", function(){
     console.log(bandCode);
     $.getJSON("http://api.bandsintown.com/events/search?"+bandCode+"&location=use_geoip&radius=20&format=json&app_id=YOUR_APP_ID", function(response){
       console.log(response);
-    })
+      console.log(response[0]);
+      var events = [];
+      for (i = 0; i < response.length; i ++)
+        {events.push(response[i]["ticket_url"])}
+      console.log(events)
+  })
 })
 
 // $("div.artist-name.ellipsis").change(function(){
