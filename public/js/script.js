34f1277eb2bc990f6ccab1d4a7129f63dbeb998c
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-08-24 21:25:46 -0400

    added multi-artist functionality and song count input

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,14 @@
 $("#makePlaylist").on("click", function(){
     $(".currentArtist").children().remove()
     var artist = $("#artistInput").val()
-    $.getJSON("http://developer.echonest.com/api/v4/playlist/basic?api_key=6N51VGIQONFDX0AGP&artist="+artist+"&format=json&results=20&bucket=tracks&bucket=id:spotify", function(response){
+    var artist = artist.split(", ")
+    var artistCode = "";
+    for (i = 0; i < artist.length; i++) {
+      artistCode += "&artist="+artist[i].split(' ').join('+');
+    }
+    console.log(artistCode);
+    var songCount = $("#songCount").val()
+    $.getJSON("http://developer.echonest.com/api/v4/playlist/basic?api_key=6N51VGIQONFDX0AGP"+artistCode+"&format=json&results="+songCount+"&bucket=tracks&bucket=id:spotify", function(response){
         console.log(response);
         tracks = [];
         for(i=0;i < response.response.songs.length; i++){
@@ -12,9 +19,7 @@ $("#makePlaylist").on("click", function(){
             tracks.push(newString);
         }
         var ids = tracks.join()
-
-        $(".currentArtist").append('<iframe src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:'+ids+'" frameborder="0" height="800" width="400" allowtransparency="true"></iframe>')
-
+        $(".currentArtist").append('<iframe id="musicframe" src="https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:'+ids+'" frameborder="0" height="800" width="400" allowtransparency="true"></iframe>')
     })
 
 
@@ -29,8 +34,4 @@ $("div.artist-name.ellipsis").change(function(){
     $.getJSON("http://developer.echonest.com/api/v4/artist/news?api_key=6N51VGIQONFDX0AGP&name="+artist+"&format=json", function(response){
         console.log(response);
     })
-
 })
-
-//   http://developer.echonest.com/api/v4/playlist/basic?api_key=6N51VGIQONFDX0AGP&artist="+artist+"&format=json&results=20&type=artist-radio&bucket=tracks&bucket=id:spotify"
-//6N51VGIQONFDX0AGP
